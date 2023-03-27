ec9d8161da34eed1d2b3dd5e87490242690fdf1f
Acer <codecinnpro@gmail.com>
2023-03-27 17:35:36 +0800

    second commit

---
+++ index.js
@@ -0,0 +1,26 @@
+let name = "fariq"
+let no = "22"
+let no1 ="20"
+
+console.log(no + no1)
+let a = parseFloat(no)
+
+console.log(typeof a)
+no.toString()
+// array
+let data= ["one", true, 23]
+console.log(data)
+
+//object
+let user1 ={
+    name: "Hee",
+    faculty: "fkekk",
+    phone: 0123456,
+    email: "kobe9systems@gmail.com",
+    comment: {name:"Lee", review: "A good friend", rating: 8}
+}
+
+console.log(user1)
+console.log(user1.comment)
+
+ console.log(user1.comment.name)
